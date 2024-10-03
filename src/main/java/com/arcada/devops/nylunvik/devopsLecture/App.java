a9754afe5dd5ac9db547a5dc71a16133af21854b
Viktor Nylund <nylunvik@arcada.fi>
2024-10-03 13:38:08 +0300

    finished unit test and fixed if conditions

--- src/main/java/com/arcada/devops/nylunvik/devopsLecture/App.java
+++ src/main/java/com/arcada/devops/nylunvik/devopsLecture/App.java
@@ -1,107 +1,58 @@
 package com.arcada.devops.nylunvik.devopsLecture;
 
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
+import java.util.Scanner;
+import java.util.ArrayList;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
-
-public final class App 
-{
-	private static int count = 0;
-    public static final void main(final  String[] args )
-    {
-    	App app = new App();
-    	app.createServer();
-    }//main closes
-    
-    public boolean createServer()
-    {
-    	try
-    	{
-    		final HttpServer server = HttpServer.create(new InetSocketAddress(8008), 0);
-            server.createContext("/", new DemoHandler()); 
-            server.setExecutor(null); // creates a default executor
-            server.start();
-            System.out.println("Hello. Your server has started. It is running on this port: http://localhost:8008/");
-                        
-            return true;
-    	}
-    	catch(final IOException exception)
-    	{
-    		exception.printStackTrace();
-    		return false;
-    	}
-    }
-    
-    private static final class DemoHandler implements HttpHandler 
-    {
-        @Override
-        public final void handle(final HttpExchange t) throws IOException 
-        {
-            final String response = "<html lang='fi'>"
-            		+ "<style>table, td {"
-            		+ "border:1px solid black;"
-            		+ "} </style>"
-            		+ "<head> <title>Pulla</title> "
-            		+ "<meta http-equiv='Content-Type' content='text/html;' /> </head>"
-            		+ "<body>"
-            		+ "<h2> Welcome to Viktor's server! "
-            		+ "The server code was provided by Niraj. <br>"
-            		+ "This page was called "+ (++count)+" times. </h2><br><br>" //extra
-            		+ "<dir style='background-color:lightgreen;'>"
-            		+ "<h1>Korvapuusti resepti:</h1>"
-            		+ "<table>"
-	            		+ "<tr>"
-	            			+ "<th>Aine</th>"
-	            			+ "<th>Osa</th>"
-            			+ "</tr><tr>"
-		            		+ "<td>hiivaa </td>"
-		            		+ "<td>50 g </td>"
-	            		+ "</tr><tr>"
-	            			+ "<td>maitoa </td>"
-		            		+ "<td>5 dl </td>"
-	            		+ "</tr><tr>"
-		            		+ "<td>kanamunaa </td>"
-		            		+ "<td>2 </td>"
-	            		+ "</tr><tr>"
-		            		+ "<td>sokeria </td>"
-		            		+ "<td>2 dl + 1 dl </td>"
-	            		+ "</tr><tr>"
-		            		+ "<td>suolaa </td>"
-		            		+ "<td>2 tl </td>"
-	            		+ "</tr><tr>"
-		            		+ "<td>kardemummaa </td>"
-		            		+ "<td>1 rkl </td>"
-	            		+ "</tr><tr>"
-		            		+ "<td>vehnejauhoa </td>"
-		            		+ "<td>16 dl </td>"
-	            		+ "</tr><tr>"
-		            		+ "<td>voita </td>"
-		            		+ "<td>~175 g + 100 g </td>"
-	            		+ "</tr><tr>"
-		            		+ "<td>kanelia </td>"
-		            		+ "<td> 2 rkl </td>"
-	            		+ "</tr>"
-            		+ "</table>"
-            		+ "<p>1. Liuota hiiva ka'denla'mpo'iseen maitoon. Lisa'a' munat, sokeri, mausteet ja osa jauhoista. Sekoita taikina tasaiseksi. </p>"
-            		+ "<p>2. Alusta va'hitellen loput jauhot taikinaan, kunnes taikina irtoaa kulhon reunoista. Lisa'a' lopuksi sulatettu voi. </p>"
-            		+ "<p>3. Anna taikinan kohota liinan alla vedottomassa paikassa kaksinkertaiseksi. </p>"
-            		+ "<p>4. Kauli taikina kahdeksi 60 x 30 cm:n kokoiseksi levyksi. </p>"
-            		+ "<p>5. Levita' levyille pehmea' voita. Ripota pinnalle sokeri ja kaneli. </p>"
-            		+ "<p>6. Ka'a'ri taikinalevyt rulliksi pitka'lta' sivulta. Ja'ta' saumakohta alapuolelle. "
-            		+ "<br>Leikkaa rullat tera'va'lla' veitsella' vinottain kolmikulmaisiksi paloiksi. Nosta palat pystyyn kapea kohta ylo'spa'in. </p>"
-            		+ "<p>7. Paina sormilla kapea kohta alas po'yta'a'n saakka. Na'in leikkauspinnat nousevat ylo's. Kohota liinan alla. </p>"
-            		+ "<p>8. Voitele kohonneet puustit. Ripota pinnalle mantelirouhetta tai raesokeria. </p>"
-            		+ "<p>9. Paista uunin keskiosassa 225 asteessa n. 8 min. </p> <br>"
-            		+ "</dir>"
-    				+ "</body></html>";
-            t.sendResponseHeaders(200, response.length());
-            final OutputStream os = t.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
-        }//handle method closes
-    }//Inner class closes
-}//class App closes
+public final class App {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Type the name of a person to see a list of their friends.\n"
+				+ "The persons currently included in the data set are:\n"
+				+ "Lisa\n"
+				+ "Erik\n"
+				+ "Input:");
+		String inputName = scanner.nextLine();
+		findFriends(inputName);
+		scanner.close();
+	} //end of main()
+	
+	public static boolean findFriends(String s) {
+		
+		ArrayList<String> friendsLisa = new ArrayList<String>();
+	    friendsLisa.add("Erik");
+	    friendsLisa.add("Henrik");
+	    friendsLisa.add("Fiona");
+	    
+	    ArrayList<String> friendsErik = new ArrayList<String>();
+		friendsErik.add("Lisa");
+		friendsErik.add("Arvid");
+		
+		// is empty > true
+		if (s.isEmpty() ) {
+			System.out.println("WRONG INPUT. Input a name and press enter.");
+			return false;
+		} // Lisa
+		if (s.equals("Lisa")){
+			System.out.println("These are all of " + s + "'s friends:");
+			for (String i : friendsLisa) {
+		      System.out.println(i);
+	    	}
+			return true;
+		} // Erik
+		else if (s.equals("Erik")) {
+			System.out.println("These are all of " + s + "'s friends:");
+			for (String i : friendsErik) {
+		      System.out.println(i);
+	    	}
+			return true;
+		} // neither
+		else {
+			System.out.println(s + " is not in the data set.");
+			return false;
+		}
+	} //end of findFriends()
+	
+}
--- src/test/java/com/arcada/devops/nylunvik/devopsLecture/AppTest.java
+++ src/test/java/com/arcada/devops/nylunvik/devopsLecture/AppTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.After;
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AppTest
 {
@@ -22,11 +23,29 @@ public class AppTest
 
 	}
 	
-	@Test
-	public void testCreateServer()
+	@Test // should be ! empty
+	public void testFindFriendsEmpty()
 	{
-		System.out.println("V har testat om de funkar!");
-		assertTrue(app.createServer());
+		System.out.println("We have run some tests!");
+		assertFalse(App.findFriends(""));
+	}
+	
+	@Test
+	public void testFindFriendsErik()
+	{
+		assertTrue(App.findFriends("Erik"));
+	}
+	
+	@Test
+	public void testFindFriendsLisa()
+	{
+		assertTrue(App.findFriends("Lisa"));
+	}
+	
+	@Test // should be ! missing
+	public void testFindFriendsNone()
+	{
+		assertFalse(App.findFriends("asdasd"));
 	}
 
 }
